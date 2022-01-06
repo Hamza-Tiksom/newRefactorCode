7c98a8cb7c7e064ef685b33cebc8fdd0912efeaf
hamza-tiksom <hamza.hanif@tiksom.com>
2022-01-06 18:59:43 +0500

    day 1

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  root 'posts#new'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts
+end
