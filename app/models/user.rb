82e126d39e68f2c6d1c89760a2168d24872d1695
hamza-tiksom <hamza.hanif@tiksom.com>
2022-01-11 19:07:29 +0500

    third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :posts ,dependent: :destroy
   has_many :comments ,dependent: :destroy
   has_one :role
-  belongs_to :admin_user
 
   after_create :assigned_default_method
 
